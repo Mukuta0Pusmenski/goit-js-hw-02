14732623accfc416ba649ff8ecfdc4147d4c50f7
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2024-09-26 01:29:42 +0300

    test1

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,16 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
 
-  if (totalPrice > customerCredits) {
-    return "Insufficient funds!";
-  } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  }
-}
+//   const totalPrice = quantity * pricePerDroid;
 
-console.log(makeTransaction(5, 1000, 6000)); // Очікуваний результат: "You ordered 5 droids worth 5000 credits!"
+
+//   if (totalPrice > customerCredits) {
+//     return "Insufficient funds!";
+//   } else {
+//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+//   }
+// }
+
+// console.log(makeTransaction(5, 1000, 6000));
+// console.log(makeTransaction(3, 1500, 4000));
+
+console.log(5);
